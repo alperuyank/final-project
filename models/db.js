d175a8e913231b4f09ee6395bc7878b43b735174
Alper Uyanik <alperuyanik4342@gmail.com>
2024-06-08 14:30:58 +0300

    db

--- models/db.js
+++ models/db.js
@@ -8,11 +8,11 @@ const dbHost = process.env.DB_HOST;
 const dbPort = process.env.DB_PORT;
 
 const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  user: dbUsername,
+  password: dbPassword,
+  host: dbHost,
+  database: dbName,
+  port: dbPort,
 });
 
 module.exports = {
